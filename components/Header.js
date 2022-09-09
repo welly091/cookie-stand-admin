86db9d16e32c7cfd94d1f0d9ff030f6451fd1e1b
welly091 <whtp1490@gmail.com>
2022-09-08 21:29:02 -0700

    lab38 finished

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+      <header className='bg-emerald-500 p-3 flex justify-between'>
+        <div className="text-2xl font-medium inline">Cookie Stand Admin</div>
+        <button className="bg-emerald-50 rounded px-2 my-1 h-5 text-xs font-medium ">Overview</button>
+      </header>
+    )
+  }
---
+++ components/ReportTable.js
@@ -0,0 +1,61 @@
+export default function ReportTable(props){
+  if (Object.keys(props.reports).length  <= 0){
+    return (
+      <h2 className='text-md font-medium p-2'>No Cookie Stands Available</h2>
+    )
+  }else{
+    //Calculate sum for each hour for all location
+    let sum_hourly_list = []
+    let total_sum = 0
+    for(let i = 0; i < 14; i++){
+      let sum = 0
+      for(let j = 0; j < props.reports.length; j++){
+        sum += props.reports[j].hourly_sales[i]
+        
+      }
+      total_sum += sum
+      sum_hourly_list[i]=sum
+    }
+    sum_hourly_list.push(total_sum)
+
+    return( 
+      <div className="flex flex-col items-center">
+        <table className="w-1/2 my-3 rounded">
+          <thead>
+            <tr className="bg-emerald-500 text-sm">
+              <th className="px-8">Location</th>
+              {props.hours.map( (hour,index) =>(
+                <th className="px-2" key={index}>{hour}</th>
+              ))}
+              <th className="px-3">Totals</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                props.reports.map((each_location, index) =>(
+                  <tr className={"text-sm" + (index % 2 === 0 ? " bg-emerald-400": " bg-emerald-300" )} key={index}> 
+                      <td className=" font-normal text-left border border-black font-semibold">&nbsp;&nbsp;{each_location.location}</td>
+                        {each_location.hourly_sales.map((sale,index) =>(
+                            <td className="px-2 border border-black font-semibold" key={index}>{sale}</td>
+                        ))}
+                      <td className="px-3 font-normal border border-black font-semibold">{each_location.hourly_sales.reduce((accumulator, currentValue) => accumulator + currentValue)}</td>
+                    </tr>
+                )
+              )
+            }
+          </tbody>
+          <tfoot>
+            <tr>
+              <td className={"border border-black text-sm font-bold text-center"+ (sum_hourly_list.length % 2 === 1 ? " bg-emerald-400": " bg-emerald-300" )}>Total</td>
+              {
+                sum_hourly_list.map((each_sum, index) =>(
+                  <td className={"border border-black text-sm font-bold text-center "+ (sum_hourly_list.length % 2 === 1 ? " bg-emerald-400": " bg-emerald-300" )} key={index}>{each_sum}</td>
+                ))
+              }
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    )
+    }
+  }
